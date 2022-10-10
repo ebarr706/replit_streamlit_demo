110b695d2bf8c90de2731f5bc03907bd71279494
Eoghan Barr <111333349+ebarr706@users.noreply.github.com>
2022-10-10 21:26:25 +0000

    Moved hour filter slider to sidebar

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYC')
+st.subheader('Testing streamlit on replit')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
@@ -29,10 +30,12 @@ hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
 # Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
+with st.sidebar:
+  hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
 
 st.subheader('-END-')
+
